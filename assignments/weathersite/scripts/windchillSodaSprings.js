8bbee9871e095e87a95f049f4d88ad3149d799ae
holmes-nicholas <holmes18@byui.edu>
2019-03-20 15:41:26 -0400

    L11 Weather Site

---
+++ assignments/weathersite/scripts/windchillSodaSprings.js
@@ -0,0 +1,17 @@
+let weatherWindchill = new XMLHttpRequest();
+let apiURLstring1 = "https://api.openweathermap.org/data/2.5/forecast?id=5607916&units=imperial&APPID=03c8d968ad0c2de9c8994d20265c8465";
+weatherWindchill.open('Get', apiURLstring1, true);
+weatherWindchill.send();
+
+weatherWindchill.onload = function() {
+    let windchillData = JSON.parse(weatherWindchill.responseText);
+    console.log(windchillData);
+
+    var temp = windchillData.list[0].main.temp;
+    var wind = windchillData.list[0].wind.speed;
+
+    // Processing - windChill formula
+    windChill = 35.74 + 0.6215*temp + (0.4275*temp - 35.75) * Math.pow(wind, 0.16);
+
+    document.getElementById('windchill').innerHTML = "Windchill: " + windChill.toFixed(0) + "&#8457";
+}
